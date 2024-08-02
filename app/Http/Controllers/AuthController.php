4f6da803274332bd35cdaffe09e89033ab6fc284
fjavalencia <franzval123@gmail.com>
2024-08-02 16:55:38 +0800

    wip: work on existing db ihris tables to use models

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+// use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    
+    /**
+     * Handle an authentication attempt.
+     */
+
+    public function authenticate(Request $request) //: RedirectResponse
+    {
+
+        // return $request->username;
+        $credentials = $request->validate([
+            'username' => ['required'],
+            'password' => ['required'],
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+
+            return Auth::user();
+            // return redirect()->intended('dashboard');
+            // return "login success!";
+        }
+
+        return response(["message" => "The provided credentials do not match our records."], 401) ;
+        // return back()->withErrors([
+        //     'email' => 'The provided credentials do not match our records.',
+        // ])->onlyInput('email');
+    }
+
+
+    function logout()
+    {
+        Auth::logout();
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+use App\Http\Controllers\AuthController;
+
+Route::post('/auth/login', [AuthController::class, 'authenticate']);
+Route::post('/auth/logout', [AuthController::class, 'logout']);
